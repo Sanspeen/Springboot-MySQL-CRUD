20556d19cdb59ecf9342f27839d7a753a9ab3b40
SanSpeen <ktns0930@gmail.com>
2023-12-20 22:21:15 -0500

    Creating User model

---
+++ src/main/java/com/example/demo/models/UserModel.java
@@ -0,0 +1,48 @@
+package com.example.demo.models;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "user")
+public class UserModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(unique = true, nullable = false)
+    private Long id;
+    private String name;
+    private String email;
+    private Integer property;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Integer getProperty() {
+        return property;
+    }
+
+    public void setProperty(Integer property) {
+        this.property = property;
+    }
+}

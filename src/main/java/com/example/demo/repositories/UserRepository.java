8e9b0e3b672f478edf7f392d45631294f62e6560
SanSpeen <ktns0930@gmail.com>
2023-12-20 22:46:57 -0500

    Implemented method save and getAll for UserModel

--- src/main/java/com/example/demo/repositories/UserRepository.java
+++ src/main/java/com/example/demo/repositories/UserRepository.java
@@ -1,4 +1,9 @@
 package com.example.demo.repositories;
 
-public class UserRepository {
+import com.example.demo.models.UserModel;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends CrudRepository<UserModel, Long> {
 }
--- src/main/java/com/example/demo/services/UserService.java
+++ src/main/java/com/example/demo/services/UserService.java
@@ -1,4 +1,22 @@
 package com.example.demo.services;
 
+import com.example.demo.models.UserModel;
+import com.example.demo.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+
+@Service
 public class UserService {
+    @Autowired
+    UserRepository userRepository;
+
+    public ArrayList<UserModel> getUsers(){
+        return (ArrayList<UserModel>) userRepository.findAll();
+    }
+
+    public UserModel saveUser(UserModel userModel){
+        return userRepository.save(userModel);
+    }
 }
